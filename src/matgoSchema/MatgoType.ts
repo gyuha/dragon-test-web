4db34551ca8f4d8705e13dea6f2d91693db7d7d0
gyuha <nicegyuha@gmail.com>
2020-07-02 01:52:11 +0900

    카드 패킷 업데이트

--- src/matgoSchema/MatgoType.ts
+++ src/matgoSchema/MatgoType.ts
@@ -2,26 +2,26 @@ export enum MessageType {
   'startGame' = 0,
   'firstPick' = 10,
   'play' = 20,
+  'error' = 30,
 }
 
 export enum RequestMessageCommand {
   'none' = 0,
   'put' = 10, //! 카드 내기
-  'backTake' = 10, //! 바닥 카드 뒤지기
-  'goStop' = 20, //! 고 or 스톱
-  'gookjin' = 30, //! 국진
-  'shake' = 40, //! 흔들기
-  'handCards' = 50, //! 손에 든 카드 확인
+  'select' = 20, //! 선택하기
+  'handCards' = 30, //! 손에 든 카드 확인
+  'goStop' = 40, //! 고 or 스톱
+  'gookjin' = 50, //! 국진
+  'shake' = 60, //! 흔들기
 }
 
 export enum ResponseMessageCommand {
   'none' = 0,
   'take' = 10, //! 카드 받아가기
-  'backTake' = 20, //! 카드
-  'selectCard' = 30, //! 카드 선택하기
-  'handCards' = 40, //! 손에 있을 카드 조회
-  'ppuk' = 50, //! 뻑
-  'gostop' = 60, //! 스톱
-  'kookjin' = 70, //! 국진
+  'select' = 20, //! 카드 선택하기
+  'handCards' = 30, //! 손에 있을 카드 조회
+  'gostop' = 40, //! 스톱
+  'gookjin' = 50, //! 국진
+  'shake' = 60, //! 국진
   'error' = 100, //! 결과 종류
 }
--- src/matgoSchema/ResponseMessage.ts
+++ src/matgoSchema/ResponseMessage.ts
@@ -9,13 +9,13 @@ import { MatgoCard } from './MatgoCard';
 import { ResponseMessageCommand } from './MatgoType';
 
 export interface ResponseMessage {
-    sessionId: string;
-    command: ResponseMessageCommand;
-    value: number[];
-    cards: MatgoCard[];
-    putCards: MatgoCard[];
-    takCards: MatgoCard[];
-    playerFloorCards: MatgoCard[];
-    message: string;
-    nextPlayerId: string;
+  sessionId?: string; //! 클라이언트 아이디, 비어 있으면 공통
+  command?: ResponseMessageCommand;
+  value?: number[]; //! 값일 경우
+  cards?: MatgoCard[]; //! 카드 들
+  playCards?: MatgoCard[]; //! 게임 진행한 카드
+  putCards?: MatgoCard[]; //! 낸 카드
+  playerFloorCards?: MatgoCard[]; //! 플레이어의 바닥 카드
+  message?: string; //! 메시지
+  nextSessionId?: string; //! 다음 턴 사용자 아이디
 }
